79e4e9414a3eba0adc43eac5a332ca0867eef9bd
Wombosvideo <busfany@hotmail.com>
2023-02-02 17:29:24 +0000

    Fix color

--- designs.js
+++ designs.js
@@ -26,6 +26,7 @@ function makeGrid() {
 
 // Generate WLED JSON code for the current grid
 function generateWLED() {
+  const rgba2hex = (rgba) => `${rgba.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/).slice(1).map((n, i) => (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n)).toString(16).padStart(2, '0').replace('NaN', '')).join('')}`
   var table = document.getElementById("pixel_canvas");
   var output = `{"on":true,"bri":100,"seg":{"i":[`;
   for (var i = 0; i < input_rows; i++) {
@@ -33,9 +34,10 @@ function generateWLED() {
       var cell = table.rows[i].cells[j];
       var color = cell.style.backgroundColor;
       if (color == "") {
-        color = "#000000";
+        color = "rgb(0, 0, 0)";
       }
-      output += `''${color.substring(1)}''`;
+      color = rgba2hex(color);
+      output += `''${color}''`;
       if (i != input_rows - 1 || j != input_cols - 1) {
         output += ",";
       }
